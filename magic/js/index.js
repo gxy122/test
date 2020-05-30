c215427c68c6f0fc5cb35288a92ad2b57e29f96b
gxy122 <gxy_122@126.com>
2020-05-30 14:00:30 +0800

    第一版完整

--- magic/js/index.js
+++ magic/js/index.js
@@ -1,4 +1,10 @@
-
+function scro() {
+    if ($(document).scrollTop() > 300) {
+        $(".comTop").fadeIn()
+    } else {
+        $(".comTop").fadeOut()
+    }
+}
 $(function(){
     $(window).resize(function(event) {
         /* Act on the event */
@@ -10,4 +16,13 @@ $(function(){
     $('.nav li').mouseleave(function(){
         $(this).find('ol').stop().slideUp();
     });
+
+    $(window).scroll(function () {
+        scro();
+    });
+    $(document).on("click", ".comTop", function () {
+        $("html,body").animate({
+            "scrollTop": 0
+        }, 500)
+    })
 });
