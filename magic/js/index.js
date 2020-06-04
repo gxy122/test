d514b587603ec9afa44485a685866d07b005c651
gxy122 <gxy_122@126.com>
2020-06-04 13:36:17 +0800

    响应式第一次调整完成

--- magic/js/index.js
+++ magic/js/index.js
@@ -24,5 +24,31 @@ $(function(){
         $("html,body").animate({
             "scrollTop": 0
         }, 500)
-    })
+    });
+
+
+    //移动端搜索
+    $(document).on('click','.search_a',function(e){
+        e.stopPropagation();
+        $('.search').stop().slideToggle();
+    });
+    $(document).on('click','.headerTop .search .box',function(e){
+        e.stopPropagation();
+    });
+    $(document).click(function(){
+        $('.search').stop().slideUp();
+    });
+
+    //导航
+    $(document).on('click','.nav_m li',function(){
+        $(this).find('ol').stop().slideToggle().closest('li').siblings().find('ol').stop().slideUp();
+    });
+    $(document).on('click','.nav_a',function(){
+        $(this).toggleClass('on');
+        if($(this).hasClass('on')){
+            $('.nav_m').stop().slideDown();
+        }else{
+            $('.nav_m').stop().slideUp();
+        }
+    });
 });
